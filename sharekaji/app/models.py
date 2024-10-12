d8bfca533e75c8cd2c5a5a15b464d2aac3891b44
yui-yashiro <yu1yu1kyklove@gmail.com>
2024-10-12 18:08:58 +0900

    新規アカウント登録画面の作成

--- sharekaji/app/models.py
+++ sharekaji/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    date_joined = None
+    username = None
+
+    name = models.CharField(max_length=32, unique=True)
+    email = models.EmailField(max_length=256, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',
+        blank=True
+    )
+
+    USERNAME_FIELD = "name"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        db_table = "users"
+
+
